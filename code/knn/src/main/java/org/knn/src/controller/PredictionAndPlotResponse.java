dcf4b10cdebf3cff0694bfe9491a16297c12465d
NIMA <linux.vps.vpn@gmail.com>
2024-09-28 00:10:53 -0700

    add lombok

--- code/knn/src/main/java/org/knn/src/controller/PredictionAndPlotResponse.java
+++ code/knn/src/main/java/org/knn/src/controller/PredictionAndPlotResponse.java
@@ -1,33 +1,21 @@
 package org.knn.src.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
 
+@AllArgsConstructor
+@Data
 public class PredictionAndPlotResponse implements Serializable {
     @Serial
     private static final long serialVersionUID = -674419265506455671L;
     private List<CashPrediction> predictions;
     private String plotImage;
 
-    public PredictionAndPlotResponse(List<CashPrediction> predictions, String plotImage) {
-        this.predictions = predictions;
-        this.plotImage = plotImage;
-    }
 
-    public List<CashPrediction> getPredictions() {
-        return predictions;
-    }
 
-    public void setPredictions(List<CashPrediction> predictions) {
-        this.predictions = predictions;
-    }
 
-    public String getPlotImage() {
-        return plotImage;
-    }
-
-    public void setPlotImage(String plotImage) {
-        this.plotImage = plotImage;
-    }
 }
