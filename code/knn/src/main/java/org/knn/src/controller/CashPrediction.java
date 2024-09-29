9d07aba97a8df2ebbfae81f144cfbe96b6119fd0
NIMA <linux.vps.vpn@gmail.com>
2024-09-28 23:38:56 -0700

    add round value and lombok

--- code/knn/src/main/java/org/knn/src/controller/CashPrediction.java
+++ code/knn/src/main/java/org/knn/src/controller/CashPrediction.java
@@ -1,24 +1,18 @@
 package org.knn.src.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
 import java.io.Serial;
 import java.io.Serializable;
 
+
+@AllArgsConstructor
+@Data
 public class CashPrediction implements Serializable {
     @Serial
     private static final long serialVersionUID = 4595018540952157928L;
     private double actualCash;
     private double predictedCash;
-
-    public CashPrediction(double actualCash, double predictedCash) {
-        this.actualCash = actualCash;
-        this.predictedCash = predictedCash;
-    }
-
-    public double getActualCash() {
-        return actualCash;
-    }
-
-    public double getPredictedCash() {
-        return predictedCash;
-    }
 }
