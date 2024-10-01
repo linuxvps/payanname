adc53126edfc7c6506a0a0928e213ab6b2b74000
alamshenas <linux.vps.vpn@gmail.com>
2024-10-01 09:56:19 +0330

    add chunck

--- code/knn/src/main/java/org/knn/src/controller/PredictionAndPlotResponse.java
+++ code/knn/src/main/java/org/knn/src/controller/PredictionAndPlotResponse.java
@@ -13,8 +13,7 @@ public class PredictionAndPlotResponse implements Serializable {
     @Serial
     private static final long serialVersionUID = -674419265506455671L;
     private List<CashPrediction> predictions;
-    private String plotImage;
-
+    List<String> chunks;
 
 
 
