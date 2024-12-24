5daceb65fdf7fe8583e1ad923b872fd36c237bdb
nima <linux.vps.vpn@gmail.com>
2024-12-24 23:54:46 +0330

    '1'

--- code/knn/src/main/java/org/knn/repository/TerminalRepository.java
+++
@@ -1,9 +0,0 @@
-package org.knn.repository;
-
-import org.knn.entity.Terminal;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TerminalRepository extends JpaRepository<Terminal, Long> {
-}
